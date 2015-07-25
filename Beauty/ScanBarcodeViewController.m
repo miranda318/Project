773f16cb674fa62aae8eb05ac377a0ef6ae64a07
jiaziw <wujiazi213@gmail.com>
2015-07-24 23:27:57 -0400

    scan

--- Beauty/ScanBarcodeViewController.m
+++ Beauty/ScanBarcodeViewController.m
@@ -14,7 +14,7 @@
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *rescanButton;
 @property (weak, nonatomic) IBOutlet PlayerView *playerView;
 // The following items will appear when scan barcode success
-@property (weak, nonatomic) IBOutlet UIView *scanResultView;
+@property IBOutlet UIView *scanResultView;
 @property (weak, nonatomic) IBOutlet UIImageView *productImage;
 @property (weak, nonatomic) IBOutlet UILabel *productNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *brandLabel;
@@ -22,13 +22,51 @@
 @property (weak, nonatomic) IBOutlet PlayerView *priceLabel;
 @property (weak, nonatomic) IBOutlet UILabel *reviewNumLabel;
 
+@property AVCaptureSession* session;
+@property AVCaptureDevice* device;
+@property AVCaptureInput* input;
+@property AVCaptureMetadataOutput* output;
+@property AVCaptureVideoPreviewLayer* prevLayer;
+
+@property NSString* upc;
 @end
 
 @implementation ScanBarcodeViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _scanResultView = [[UIView alloc] init];
+    _scanResultView.autoresizingMask = UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin;
+    _scanResultView.layer.borderColor = [UIColor greenColor].CGColor;
+    _scanResultView.layer.borderWidth = 3;
+    [self.view addSubview:_scanResultView];
+    
+    _session = [[AVCaptureSession alloc] init];
+    _device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    
+    _input = [AVCaptureDeviceInput deviceInputWithDevice:_device error:&error];
+    if (_input) {
+        [_session addInput:_input];
+    } else {
+        NSLog(@"Error: %@", error);
+    }
+    
+    _output = [[AVCaptureMetadataOutput alloc] init];
+    [_output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [_session addOutput:_output];
+    
+    _output.metadataObjectTypes = [_output availableMetadataObjectTypes];
+    
+    _prevLayer = [AVCaptureVideoPreviewLayer layerWithSession:_session];
+    _prevLayer.frame = self.view.bounds;
+    _prevLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    [self.view.layer addSublayer:_prevLayer];
+    
+    [_session startRunning];
+    
+    [self.view bringSubviewToFront:_scanResultView];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,14 +74,51 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+#pragma mark - Scan
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    CGRect scanResultViewRect = CGRectZero;
+    AVMetadataMachineReadableCodeObject *barCodeObject;
+    NSString *detectionString = nil;
+    NSArray *barCodeTypes = @[AVMetadataObjectTypeUPCECode, AVMetadataObjectTypeCode39Code, AVMetadataObjectTypeCode39Mod43Code,
+                              AVMetadataObjectTypeEAN13Code, AVMetadataObjectTypeEAN8Code, AVMetadataObjectTypeCode93Code, AVMetadataObjectTypeCode128Code,
+                              AVMetadataObjectTypePDF417Code, AVMetadataObjectTypeQRCode, AVMetadataObjectTypeAztecCode];
+    
+    for (AVMetadataObject *metadata in metadataObjects) {
+        for (NSString *type in barCodeTypes) {
+            if ([metadata.type isEqualToString:type])
+            {
+                barCodeObject = (AVMetadataMachineReadableCodeObject *)[_prevLayer transformedMetadataObjectForMetadataObject:(AVMetadataMachineReadableCodeObject *)metadata];
+                scanResultViewRect = barCodeObject.bounds;
+                detectionString = [(AVMetadataMachineReadableCodeObject *)metadata stringValue];
+                break;
+            }
+        }
+        
+        if (detectionString != nil)
+        {
+            _upc = detectionString;
+            break;
+        }
+        else{
+            _upc = @"";
+        }
+        
+        NSLog(@"%@",_upc);
+        _scanResultView.frame = scanResultViewRect;
+    }
+    
 }
-*/
+
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
