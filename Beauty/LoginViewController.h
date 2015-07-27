b43605ef99c5c38489f2dadb0f3d54765720d25e
WeifengC <amazonweifengc@gmail.com>
2015-07-27 14:00:31 -0400

    LoginView
    Add loginview to the Xcode

---
+++ Beauty/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Beauty
+//
+//  Created by WeifengCui on 7/27/15.
+//  Copyright © 2015 QueensMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
