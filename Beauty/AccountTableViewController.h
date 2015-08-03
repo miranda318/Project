bc0acd25e72eeb5c11a00d2f08a37e2bc8c630cf
WeifengC <amazonweifengc@gmail.com>
2015-08-03 14:37:24 -0400

    begin to facebook login

---
+++ Beauty/AccountTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountTableViewController.h
+//  Beauty
+//
+//  Created by WeifengCui on 8/3/15.
+//  Copyright © 2015 QueensMan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountTableViewController : UITableViewController
+
+@end
